68da16244b0d78276bea5e9ffab66642f4e8eb65
AndyGarey <elsa.garnica01@gmail.com>
2019-02-25 00:47:07 -0600

    agregando estilos y modificando form

--- assets/script.js
+++ assets/script.js
@@ -2,37 +2,34 @@
 const sushiForm = document.querySelector('#sushi-form');
 const numero = document.querySelector('#numero');
 const sushi = document.querySelector('#sushi');
-const sushiRoll = document.getElementById('ingrediente')
+// const sushiRoll = document.getElementById('ingrediente');
 const lista = document.querySelector('#lista');
 
 sushiForm.addEventListener('submit', function(event){
     event.preventDefault();
     const cantidad = numero.value;
     const saborSushi = sushi.value;
-    const sushiValor = sushiRoll.value;
+    // const sushiValor = sushiRoll.value;
 
+
+// Agregando imagen al pedido
     let sushiImg
     if(saborSushi == 'salmon') {
         sushiImg = 'sushi-salmon.jpg';
-    } else {
-        sushiImg = 'lentes.png';
+    } else if(saborSushi == 'camaron'){
+        sushiImg = 'camaron.jpg';
+    } else if(saborSushi == 'atun'){
+        sushiImg = 'atun.jpg';
     }
 
+// Agregando los detalles del pedido al html
 lista.innerHTML += `
     <div class="seleccion-sushi">
-        <h3>Pidieron ${cantidad} Sushi Roll de ${saborSushi}</h3>
-        <p>Con ingrediente extra ${sushiValor}</p>
-        <img src="assets/img/${sushiImg}">
+        <h3>${cantidad} Sushi Roll de ${saborSushi}</h3>
+        <img class="img-sushiland" src="assets/img/${sushiImg}">
         <input id="confirm-button" type="button" value="confirmar">
     </div>
 `
 });
 
-// console.log(lentesForm);
-// console.log(lentes.value);  // se manda llamar el valor del input lentes
-// lentesForm.addEventListener('submit', function(event){
-//     event.preventDefault();        // callback agregamos el evento y detiene el submit
-//     console.log('hemos detenido el submit');
-//     const estiloLentes = lentes.value;     // guarda el estilo de lentes que se desea pedir
-//     console.log(estiloLentes);             // lista.innerHTML = 'Tus hermosos lentes son' + estiloLentes;
-//     lista.innerHTML += `<p> Tus hermosos lentes son: ${estiloLentes}</p>`;
+        // <p>Con ingrediente extra ${sushiValor}</p>
